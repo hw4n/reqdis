7d65c82a09dc2602a2d413ce94a3e33e355e9a4b
Hwanhee Chae <hh@hwan.me>
2024-07-04 00:04:13 +0900

    add css

--- client/script.js
+++ client/script.js
@@ -1,16 +1,32 @@
 let messageData = [];
 
+function randomColor() {
+    let r = Math.floor(Math.random() * 71);
+    let g = Math.floor(Math.random() * 71);
+    let b = Math.floor(Math.random() * 71);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 function createElements(message) {
-    return message.map(m => `<div class="message">${m.author} : ${m.message}</div>`).join('');
+    return message.map(m => `<div class="message" style="background-color: ${randomColor()}">${m.author} : ${m.message}</div>`).join('');
 }
 
 setInterval(() => {
-    fetch('http://localhost:4343/msg')
+    fetch('https://yapi.hwan.me/msg')
         .then(response => response.json())
         .then(data => {
-            if (messageData.length !== data.length) {
-                messageData = [...data];
+            if (messageData.length === 0) {
                 document.getElementById('container').innerHTML = createElements(data);
+                messageData = data.slice();
+            }
+
+            if (messageData.length !== data.length) {
+                console.log(messageData);
+                console.log(data);
+                console.log('----------')
+                let newEntries = data.slice(messageData.length);
+                document.getElementById('container').innerHTML += createElements(newEntries);
+                messageData.push(...newEntries);
             }
         });
 }, 1000);
