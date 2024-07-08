9b93648d2ae88928b125c553fbce16b1326bc2de
Hwanhee Chae <hh@hwan.me>
2024-07-08 16:59:02 +0900

    fix replaceAll

--- server/helper/screener.js
+++ server/helper/screener.js
@@ -4,15 +4,19 @@ const fs = require('fs');
 
 class screener {
     static censorMessage(originalMessage) {
-        var pureMessage = originalMessage.replace('/[!@#$%^&*()_+ ,.]/g', '');
+        var pureMessage = originalMessage.replaceAll(/[!@#$%^&*()_+ ,./\[\]]/gi, '');
+        console.log(originalMessage, "/", pureMessage);
         var processedMessage = pureMessage;
         const curseDB = JSON.parse(fs.readFileSync('./static/curse.json', 'utf8'));
+
+        console.log(pureMessage, processedMessage, originalMessage)
 
         for (let filteredWord in curseDB) {
             for (let curseWord of curseDB[filteredWord]) {
                 processedMessage = processedMessage.replace(new RegExp(curseWord, 'gi'), filteredWord);
             }
         }
+        console.log(pureMessage, processedMessage, originalMessage)
         return processedMessage === pureMessage ? originalMessage : processedMessage;
     }
 }
